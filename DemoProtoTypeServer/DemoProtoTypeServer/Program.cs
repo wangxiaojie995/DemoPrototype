b139ca193fa44dabdd90e41c03d9d8ba67b6da95
王晓洁 <867778117@qq.com>
2018-01-11 22:16:03 +0800

    first commit

---
+++ DemoProtoTypeServer/DemoProtoTypeServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace DemoProtoTypeServer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Socket 
+        }
+    }
+}
